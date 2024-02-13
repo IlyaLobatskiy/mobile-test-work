b187a0e33f37924ae58a7d38b6283c7a2142b796
Ilya Lobatskiy <118415552+IlyaLobatskiy@users.noreply.github.com>
2024-02-13 14:06:40 +0700

    Update BasePage.java

--- src/main/java/org/my/test/app/framework/BasePage.java
+++ src/main/java/org/my/test/app/framework/BasePage.java
@@ -1,8 +1,14 @@
 package org.my.test.app.framework;
 
+import org.my.test.app.managers.DriverManager;
+import org.my.test.app.managers.PageManager;
+import org.my.test.app.managers.TestPropManager;
 import org.openqa.selenium.WebElement;
 
 public class BasePage {
+    protected final DriverManager driver = DriverManager.getDriverManager();
+    protected final PageManager pageManager = PageManager.getPageManager();
+    protected final TestPropManager prop = TestPropManager.getTestPropManager();
 
     /**
      * Проверяет нахождение элемента на странице
@@ -19,7 +25,6 @@ public class BasePage {
      * @param element - путь к полю ввода
      * @param content - контент которым заполняется поле
      */
-
     public void fillingInInputField(WebElement element, String content) {
         element.sendKeys(content);
     }
@@ -29,7 +34,6 @@ public class BasePage {
      *
      * @param element - Элемент на который нажимаем
      */
-
     public void clickElement(WebElement element) {
         element.click();
     }
@@ -39,7 +43,6 @@ public class BasePage {
      *
      * @param element - элемент текст которого возвращаем
      */
-
     public String getTextElement(WebElement element) {
         return element.getText();
     }
